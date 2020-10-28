8b2c85b027ee15e14aeb1568528d9e6204f81384
VentusDominus-sys <32097339+ImrVentus@users.noreply.github.com>
2020-10-28 18:17:04 +0200

    Add files via upload

---
+++ Скакодуб Кирило/Lab 1.2.6 (1) Your first program [A] Скакодуб/1st porgramm/1st porgramm.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <locale.h>
+using namespace std;
+int main(void) {
+	setlocale(LC_ALL, "Russian");
+	cout << "Привет! :D\n";
+	cout << "И тебе привет! ;)" << endl;
+	cout << endl;
+	cout << "Эт я, твоя первая программа!" << endl;
+	return 0;
+}
